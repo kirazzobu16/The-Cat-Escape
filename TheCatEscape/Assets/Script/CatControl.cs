9c8a6ff312e496e0ff800cabed740c5895a61bae
kirazzobu16 <37017093+kirazzobu16@users.noreply.github.com>
2021-10-27 15:23:12 +0300

    düzeltme yapıldı

--- TheCatEscape/Assets/Script/CatControl.cs
+++ TheCatEscape/Assets/Script/CatControl.cs
@@ -1,45 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CatControl : MonoBehaviour
 {
-    Rigidbody rb;
-    public Movement movement;
-    public Rotate rotate;
-    public VariableJoystick variableJoystick;
-    public GameObject karakter;
-    public float Speed;
-    public bool up=false;
-    public bool left = false;
-    public bool right = false;
-    public bool down = false;
-    public Vector2 direction;
-    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-    // void Update()
-    //{
-    //    //karakter.transform.position += new Vector3(1,0)*Speed;
 
-    //    direction = variableJoystick.Direction;
-    //}
-    public void MoveWithJoystick()
+        
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-
-    }
- /// <summary>
- /// K
- /// </summary>
- /// <param name="sprite"></param>
-    public void ChangeSprite(Sprite sprite)
-    {
-        if (sprite == null||GameManager.Instance.isMoving)
-            return;
-        spriteRenderer.sprite = sprite;
-    }
-
+    
+    } 
 }
-
